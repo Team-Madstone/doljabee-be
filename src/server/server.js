c605b13530c4c4e6ed8eb183929d7b497df7aa57
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-11 14:05:25 +0900

    🔧 morgan 설정

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,17 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
+const logger = morgan('dev');
 
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(logger);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
