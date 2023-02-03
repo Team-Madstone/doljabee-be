c1c9f6b2e0d9d086c69b070ece957190934782a2
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-03 15:10:21 +0900

    ✨ userController 추가

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,7 +5,11 @@ import bcrypt from 'bcrypt';
 import { signup } from '../mail/signup';
 import { transporter } from '../utils/mail';
 import jwt from 'jsonwebtoken';
-import { TCreateUserVariables, TSendVerifyEmail } from '../types/user';
+import {
+  TCreateUserVariables,
+  TSendVerifyEmail,
+  TTokenPayload,
+} from '../types/user';
 
 const SALT_ROUND = 10;
 
@@ -56,5 +60,18 @@ export const createUser = async (
   } catch (error) {
     res.status(500).send({ message: DEFAULT_ERROR_MESSAGE });
     return;
+  }
+};
+
+export const verifyUser = async (req: Request, res: Response) => {
+  const token = req.query.token as string;
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET) as TTokenPayload;
+
+    const user = await User.findOne({ email: payload.email });
+    await user.updateOne({ $set: { verifyEmail: true } });
+    res.status(200).send({ message: '인증이 완료되었습니다.' });
+  } catch (error) {
+    res.status(405).send({ message: '인증에 실패했습니다.' });
   }
 };
