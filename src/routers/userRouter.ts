4cd95abd7f1ff224f02a1a9ce1756eac0b92be2d
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-24 01:44:17 +0900

    ✨ refreshAccessToken 라우터 추가

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   createUser,
   loginUser,
+  refreshAccessToken,
   verifyEmail,
 } from '../controllers/userController';
 import { validationRule } from '../middlewares/userValidationRule';
@@ -22,4 +23,6 @@ userRouter.post(
 );
 userRouter.post('/verify-email', verifyEmail);
 userRouter.post('/login', loginUser);
+userRouter.post('/refresh-access-token', refreshAccessToken);
+
 export default userRouter;
