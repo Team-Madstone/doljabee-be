dfb3ffce3658b63dbe6e9694adaeee68bd2c744b
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-24 01:06:43 +0900

    ✨ login 라우터 추가

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,5 +1,9 @@
 import express from 'express';
-import { createUser, verifyEmail } from '../controllers/userController';
+import {
+  createUser,
+  loginUser,
+  verifyEmail,
+} from '../controllers/userController';
 import { validationRule } from '../middlewares/userValidationRule';
 import { validate } from '../utils/validate';
 
@@ -17,4 +21,5 @@ userRouter.post(
   createUser
 );
 userRouter.post('/verify-email', verifyEmail);
+userRouter.post('/login', loginUser);
 export default userRouter;
