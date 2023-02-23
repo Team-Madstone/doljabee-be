98aa4356ad89d00e119a8cd03f623a4bcff46c8f
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-24 01:51:04 +0900

    ✨ getMyProfile 라우터 추가

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import {
   createUser,
+  getMyProfile,
   loginUser,
   logout,
   refreshAccessToken,
   verifyEmail,
 } from '../controllers/userController';
+import { isLogin } from '../middlewares/isLogin';
 import { validationRule } from '../middlewares/userValidationRule';
 import { validate } from '../utils/validate';
 
@@ -26,5 +28,6 @@ userRouter.post('/verify-email', verifyEmail);
 userRouter.post('/login', loginUser);
 userRouter.post('/refresh-access-token', refreshAccessToken);
 userRouter.post('/logout', logout);
+userRouter.get('/get-my-profile', isLogin, getMyProfile);
 
 export default userRouter;
