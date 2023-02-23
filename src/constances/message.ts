ce3dccf260d580e2fd204fe0a9a8b7a096f96f9b
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-24 00:08:18 +0900

    🥅 feedController 에러 처리 일괄 수정

--- src/constances/message.ts
+++ src/constances/message.ts
@@ -1,1 +1,6 @@
 export const DEFAULT_ERROR_MESSAGE = 'SERVER_ERROR';
+
+export const FAILURE = {
+  UpdateFeed: '피드 업데이트에 실패했습니다.',
+  DeleteFeed: '피드 삭제에 실패했습니다.',
+};
--- src/controllers/feedController.ts
+++ src/controllers/feedController.ts
@@ -1,75 +1,90 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { DEFAULT_ERROR_MESSAGE, FAILURE } from '../constances/message';
 import Feed from '../models/Feed';
 
 export const getFeeds = async (req: Request, res: Response) => {
-  const feeds = await Feed.find({});
   try {
-    return res.send(feeds);
+    const feeds = await Feed.find({});
+    return res.status(200).send(feeds);
   } catch (error) {
-    return res.sendStatus(404);
+    return res.status(500).send({ message: DEFAULT_ERROR_MESSAGE });
   }
 };
 
 export const getFeed = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const feed = await Feed.findById(id);
   try {
-    return res.send(feed);
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(500).send({ message: DEFAULT_ERROR_MESSAGE });
+    }
+
+    const feed = await Feed.findById(id);
+
+    return res.status(200).send(feed);
   } catch (error) {
-    return res.sendStatus(404);
+    return res.status(500).send({ message: DEFAULT_ERROR_MESSAGE });
   }
 };
 
 export const uploadFeed = async (req: Request, res: Response) => {
-  const { title, text } = req.body;
-  const { path } = req.file;
   try {
+    const { title, text } = req.body;
+    // req.file은 multer가 파일을 업로드 했을 때만 생성됨.
+    // path는 optional하기 때문에 구조분해할당 불가
+    const path = req.file?.path;
+
     const newFeed = await Feed.create({
       title,
       text,
       photo: path,
       likes: 0,
     });
-    return res.send(newFeed);
+
+    return res.status(200).send(newFeed);
   } catch (error) {
-    return res.sendStatus(400);
+    return res.status(500).send({ message: DEFAULT_ERROR_MESSAGE });
   }
 };
 
 export const updateFeed = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { title, text } = req.body;
-  const file = req.file;
-  const path = file && file.path;
-
-  const feed = await Feed.findById(id);
-  if (!feed) {
-    return res.sendStatus(404);
-  }
-
   try {
+    const { id } = req.params;
+    const { title, text } = req.body;
+    const file = req.file;
+    const path = file && file.path;
+
+    const feed = await Feed.findById(id);
+
+    if (!feed) {
+      return res.status(400).send({ message: FAILURE.UpdateFeed });
+    }
+
     const updatedFeed = await Feed.findByIdAndUpdate(id, {
       title,
       text,
       photo: path,
     });
-    return res.send(updatedFeed);
+
+    return res.status(200).send(updatedFeed);
   } catch (error) {
-    return res.sendStatus(404);
+    return res.status(500).send({ message: DEFAULT_ERROR_MESSAGE });
   }
 };
 
 export const deleteFeed = async (req: Request, res: Response) => {
-  const { _id } = req.body;
-  const feeds = await Feed.findById(_id);
-  if (!feeds) {
-    return res.sendStatus(404);
-  }
-
   try {
+    const { _id } = req.body;
+    const feeds = await Feed.findById(_id);
+
+    if (!feeds) {
+      return res.sendStatus(400).send({ message: FAILURE.DeleteFeed });
+    }
+
     await Feed.findByIdAndDelete(_id);
-    return res.sendStatus(200);
+    return res.status(200);
   } catch (error) {
-    return res.sendStatus(404);
+    return res.status(500).send({ message: DEFAULT_ERROR_MESSAGE });
   }
 };
