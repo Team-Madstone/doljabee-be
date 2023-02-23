90e44611387e65375eb511605cc86f19b52063bb
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-24 00:37:53 +0900

    ✨ 상태 메세지 추가

--- src/constances/message.ts
+++ src/constances/message.ts
@@ -1,6 +1,13 @@
 export const DEFAULT_ERROR_MESSAGE = 'SERVER_ERROR';
 
+export const SUCCESS = {
+  CreateUser: '사용자 등록이 완료되었습니다.',
+  VerifyEmail: '인증이 완료되었습니다.',
+};
+
 export const FAILURE = {
   UpdateFeed: '피드 업데이트에 실패했습니다.',
   DeleteFeed: '피드 삭제에 실패했습니다.',
+  CannotFindUser: '해당 이메일을 찾을 수 없습니다.',
+  VerifyEmail: '인증에 실패했습니다.',
 };
