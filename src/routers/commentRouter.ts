cbee3a208ce2dcf515472950ae0c44f55cb088bf
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-03-06 00:30:08 +0900

    ✨ editComment 라우터 추가

--- src/routers/commentRouter.ts
+++ src/routers/commentRouter.ts
@@ -1,5 +1,9 @@
 import express from 'express';
-import { createComment, deleteComment } from '../controllers/commentController';
+import {
+  createComment,
+  deleteComment,
+  editComment,
+} from '../controllers/commentController';
 
 import { isLogin } from '../middlewares/isLogin';
 
@@ -7,5 +11,6 @@ const commentRouter = express.Router();
 
 commentRouter.post('/', isLogin, createComment);
 commentRouter.delete('/', isLogin, deleteComment);
+commentRouter.put('/:id', isLogin, editComment);
 
 export default commentRouter;
